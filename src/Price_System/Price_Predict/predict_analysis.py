7e431a3a136f775997cf9de5f8e2f0fb24a184e4
ZhangJianquan <qq476590716@sjtu.edu.cn>
2020-02-28 16:04:36 +0800

    Upload New File

---
+++ src/Price_System/Price_Predict/predict_analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import joblib
+import shap
+
+# model_path 输入模型pkl的路径
+# name_of_model 输入模型的名字
+# data_path 训练数据集csv的路径
+# output_path 输出图片的路径 '_shap_impact.png'
+def predict_analysis(model_path, name_of_model, data_path, output_path):
+    model = joblib.load(model_path)
+    X_train = pd.read_csv(data_path)
+
+    index, shap_mean_df = shap_compute(X_train, model, name_of_model)
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.barh(index, shap_mean_df['shap_mean_values'].values)
+    ax.set_yticks(index)
+    ax.set_yticklabels(shap_mean_df['columns_name'].values)
+    ax.set_xlabel('mean(|shap_value|) average impact on model output', size=15)
+
+    plt.savefig(output_path)
+
+    return 
